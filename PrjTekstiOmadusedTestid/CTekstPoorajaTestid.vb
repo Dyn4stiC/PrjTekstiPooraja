2f799104cce439b587523390da48c672ec02a470
dyn4stic <Oliver.sild2002@gmail.com>
2024-03-06 15:07:36 +0200

    CTekstipoorajaTest

---
+++ PrjTekstiOmadusedTestid/CTekstPoorajaTestid.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class CTekstPoorajaTestid
+
+    <TestMethod()> Public Sub PooraFunktsiooniga_YksSona_VastusPooratudSona()
+        Dim pooraja As New PrjTekstiOmadused.CTekstiPooraja
+        Dim vastus = pooraja.PooraFunktsiooniga("abcxyz")
+        Assert.AreEqual(vastus, "zyxcba")
+    End Sub
+
+End Class
---
+++ PrjTekstiOmadusedTestid/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("PrjTekstiOmadusedTestid")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PrjTekstiOmadusedTestid")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("1efc8e61-bae6-4f7d-9d1d-0da0224dab7c")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
